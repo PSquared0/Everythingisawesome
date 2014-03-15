6df5c268962466c3af2eb646b0a54e0c5089b316
Your Actual NamePascal <pascaleee@gmail.com>
2014-03-15 14:23:33 -0700

    cleaned up the page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -28,7 +28,7 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.save
-        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+        format.html { redirect_to topics_path, notice: 'Topic was successfully created.' }
         format.json { render action: 'show', status: :created, location: @topic }
       else
         format.html { render action: 'new' }
@@ -42,7 +42,7 @@ class TopicsController < ApplicationController
   def update
     respond_to do |format|
       if @topic.update(topic_params)
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.html { redirect_to topic_path, notice: 'Topic was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -60,6 +60,12 @@ class TopicsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def upvote
+    @topic = Topic.find(params[:id])
+    @topic.votes.create
+    redirect_to(topics_path)
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -72,9 +78,5 @@ class TopicsController < ApplicationController
       params.require(:topic).permit(:title, :description)
     end
 
-    def upvote
-  @topic = Topic.find(params[:id])
-  @topic.votes.create
-  redirect_to(topics_path)
-end
+
 end
